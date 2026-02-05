8f3cd698f09b9b96dacd5f17795d49df11567c22
chrismalone617 <chris@m11design.com>
2026-02-05 18:43:59 -0500

    Update package dependencies for UnoCSS presets and adjust configuration for improved styling

--- astro-site/uno.config.ts
+++ astro-site/uno.config.ts
@@ -2,20 +2,19 @@
 import { defineConfig, presetIcons, presetWind4, presetTypography } from 'unocss'
 
 export default defineConfig({
- presets: [
- presetWind4({
- preflights: {
- reset: true
- }
- }),
- presetIcons({
- extraProperties: {
- display: 'inline-block',
- 'vertical-align': 'middle'
- // ...
- }
- }),
- presetTypography()
- ],
- safelist: []
+	presets: [
+		presetWind4({
+			preflights: {
+				reset: true
+			}
+		}),
+		presetIcons({
+			extraProperties: {
+				display: 'inline-block',
+				'vertical-align': 'middle'
+			}
+		}),
+		presetTypography()
+	],
+	safelist: []
 })
